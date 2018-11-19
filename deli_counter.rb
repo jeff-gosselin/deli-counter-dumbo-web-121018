42a8c44c5d1f0175b08144fee192c2a02f0a4c7a
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-19 23:41:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(arr) 
+  if arr.empty?
+    puts "The line is currently empty."
+  else
+    positions = []
+    arr.each_with_index do |customer, i|
+      positions.push("#{i + 1}. #{customer}")
+    end
+  end
+  
+  puts "The line is currently: #{positions.join(' ')}"
+end
